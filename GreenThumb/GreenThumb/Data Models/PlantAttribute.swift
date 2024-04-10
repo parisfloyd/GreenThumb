9974554f1c9311b9ee37ccdf6473b3d2ff9205f3
parisfloyd <parisfloyd@mines.edu>
2024-04-10 11:20:46 -0600

    The plots now store Plant information

---
+++ GreenThumb/GreenThumb/Data Models/PlantAttribute.swift
@@ -0,0 +1,8 @@
+//
+//  PlantAttribute.swift
+//  GreenThumb
+//
+//  Created by Paris Floyd (Student) on 4/10/24.
+//
+
+import Foundation
---
+++ GreenThumb/GreenThumb/Views/PlotDescriptionView.swift
@@ -0,0 +1,18 @@
+//
+//  PlotDescriptionView.swift
+//  GreenThumb
+//
+//  Created by Paris Floyd (Student) on 4/10/24.
+//
+
+import SwiftUI
+
+struct PlotDescriptionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlotDescriptionView()
+}
